5a2517d1310d6e7408a3ca99ee8109b6676b7b86
Wendy <wendy.c2502@gmail.com>
2023-08-02 16:40:37 -0600

    Initial Commit

---
+++ Literna/LiternaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiternaApp.swift
+//  Literna
+//
+//  Created by Wendy on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LiternaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
